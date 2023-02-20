5b944f83cd6ea40a3e040389dbe59c1695a0510b
awirthy <a.wirthy@gmail.com>
2023-02-21 06:50:32 +1100

    v0.11.0
    fix bash script

--- DownloadYouTubePlex.sh
+++ DownloadYouTubePlex.sh
@@ -1,4 +1,4 @@
 # /etc/cron.d/ytdl
 # 
-python3 /opt/DownloadYouTubev2/DownloadYouTubePlex-0.10.0/DownloadYouTubePlex.py  >> /proc/1/fd/1;
+python3 /opt/DownloadYouTubePlex/DownloadYouTubePlex-0.11.0/DownloadYouTubePlex.py  >> /proc/1/fd/1;
 echo "DONE"  >> /proc/1/fd/1;
